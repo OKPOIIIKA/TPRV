1bf5f8799887b3e46fc9a7e80d48ba3a002872aa
OKPOIIIKA <106810833+OKPOIIIKA@users.noreply.github.com>
2023-12-20 22:18:03 +0700

    lab3.cpp

--- Lab3/lab3/lab3.cpp
+++ Lab3/lab3/lab3.cpp
@@ -1,113 +1,90 @@
-#include <chrono>
-#include <cstdlib>
 #include <iostream>
-#include <mpi.h>
-#include <openssl/md5.h>
-#include <string>
+#include <iomanip>
+#include <sstream>
+#include "mpi.h"
+#include <chrono>
+#include "openssl/md5.h"
+#include <vector>
 
-std::string generate_next_password(std::string current_password)
-{
-    for (int i = current_password.length() - 1; i >= 0; i--) 
-    {
-        if (current_password[i] == '9')
-        {
-            current_password[i] = 'a';
-            return current_password;
-        }
-        else if (current_password[i] == 'z')
-        {
-            current_password[i] = 'A';
-            return current_password;
-        }
-        else if (current_password[i] == 'Z')
-        {
-            current_password[i] = '0';
-        }
-        else
-        {
-            current_password[i]++;
-            return current_password;
-        }
-    }
-    return "";
-}
+//разбиение алфавита на 7 равных частей
+std::vector<std::string> splitString(const std::string& inputString) {
+    std::vector<std::string> substrings;
+    size_t length = inputString.length();
 
-std::string calculate_md5(const std::string &input)
-{
-    unsigned char md5_hash[MD5_DIGEST_LENGTH];
-    MD5((const unsigned char *)input.c_str(), input.length(), md5_hash);
-
-    char md5_str[33];
-    for (int i = 0; i < 16; i++)
-    {
-        sprintf(&md5_str[i * 2], "%02x", (unsigned int)md5_hash[i]);
+    for (size_t i = 0; i < length; i += length / 7) {
+        substrings.push_back(inputString.substr(i, length / 7));
     }
 
-    return std::string(md5_str);
+    return substrings;
 }
 
-int main(int argc, char **argv)
-{
-    std::string password;
+//вычисление MD5-хэша
+std::string md5(const std::string& input) {
+    MD5_CTX context;
+    MD5_Init(&context);
+    MD5_Update(&context, input.c_str(), input.length());
+
+    unsigned char result[MD5_DIGEST_LENGTH];
+    MD5_Final(result, &context);
+
+    std::stringstream md5Stream;
+    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i) {
+        md5Stream << std::hex << std::setw(2) << std::setfill('0') << (int)result[i];
+    }
+
+    return md5Stream.str();
+}
+//следующий пароль для проверки
+std::string get_next_password(std::string current_password, std::string alphabet, int letter_num, int num) {
+    current_password[letter_num] = alphabet[num];
+    return current_password;
+}
+
+int main(int argc, char **argv) {
 
     MPI_Init(&argc, &argv);
 
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    std::string alphabet = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+    std::vector<std::string> chunks = splitString(alphabet);
 
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int rank_id, num_proc;
+    int finished = 0;
+    const int alphabet_len = alphabet.length() - 1;
 
-    int password_length = 5;
+    MPI_Status status;
+    MPI_Request request;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank_id);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
 
     auto start = std::chrono::high_resolution_clock::now();
-
-    std::string target_md5 =
-        "ced457730b69e5dbb59441fb1b08d019";
-
-    std::string current_password = "";
-    for (int i = 0; i < password_length; i++)
-    {
-        current_password += '0';
-    }
-
-    long long iterations = 0;
-
-    bool password_found = false;
-
-    while (!password_found)
-    {
-        std::string md5 = calculate_md5(current_password);
-        iterations++;
-
-        if (md5 == target_md5)
-        {
-            password = current_password;
-            password_found = true;
-        }
-
-        if (world_rank == 0)
-        {
-            current_password = generate_next_password(current_password);
-        }
-
-        MPI_Bcast(&current_password[0], password_length, MPI_CHAR, 0,
-                  MPI_COMM_WORLD);
-
-        if (password_found)
-        {
-            int finished = 1;
-            MPI_Bcast(&finished, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    std::string init = "QQQQQ";
+    std::string word = "W43xs";//"vA7ny";
+    std::string word_hash = md5(word);
+    for (uint i = 0; finished != 1; i++) {
+        for (int frst = 0; frst < chunks[rank_id].length() - 1; frst++) {
+            for (int scnd = 0; scnd < alphabet_len; scnd++) {
+                for (int thrd = 0; thrd < alphabet_len; thrd++) {
+                    for (int fth = 0; fth < alphabet_len; fth++) {
+                        for (int ffth = 0; ffth < alphabet_len; ffth++) {
+                            init[0] = chunks[rank_id][frst];
+                            init[1] = alphabet[scnd];
+                            init[2] = alphabet[thrd];
+                            init[3] = alphabet[fth];
+                            init[4] = alphabet[ffth];
+                            if (md5(init) == word_hash) {
+                                finished = 1;
+                                auto end = std::chrono::high_resolution_clock::now();
+                                auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+                                std::cout << "Runk: " << rank_id << " found the word: " << init << " for time: " << duration <<'\n';
+                                MPI_Abort(MPI_COMM_WORLD, 0);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
-
-    if (world_rank == 0)
-    {
-        auto stop = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-        std::cout << "Password found: " << password << "\n";
-        std::cout << "Time: " << duration.count() << " milliseconds\n";
-    }
+    std::cout << "Runk: " << rank_id << " finished: " << finished << " word: " << word << "\n";
 
     MPI_Finalize();
     return 0;
